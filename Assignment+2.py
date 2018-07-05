1a741d2edddd214665f3238de045095845c7cf3d
xuteng <xuteng0220@gmail.com>
2018-07-05 21:20:59 +0800

    solve conflict

--- Assignment+2.py
+++ Assignment+2.py
@@ -100,3 +100,12 @@ answer_five3()
 def answer_six():
     popNum = census_df.set_index('STNAME').groupby(level = 0)['CENSUS2010POP'].agg({'CENSUS2010POP': np.sum})
     return popNum.sort_values('CENSUS2010POP', ascending=False)[0:3].index
+
+
+
+def answer_seven():
+    counties_df = census_df[census_df['SUMLEV'] == 50]
+    counties_df['changePop'] = abs(counties_df['POPESTIMATE2011'] - counties_df['POPESTIMATE2010']) + abs(counties_df['POPESTIMATE2012'] - counties_df['POPESTIMATE2011']) + abs(counties_df['POPESTIMATE2013'] - counties_df['POPESTIMATE2012']) + abs(counties_df['POPESTIMATE2014'] - counties_df['POPESTIMATE2013']) + abs(counties_df['POPESTIMATE2015'] - counties_df['POPESTIMATE2014'])
+    return counties_df.sort_values(by = 'changePop', ascending = False)
+a = answer_seven()
+a[0]['COUNTY']
